d74f4789884bdb7ab3e77617e284c392b9d8fed5
Joel Da Rosa Junior <jr.dasrosas@gmail.com>
2023-01-09 15:20:05 -0300

    update

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import subprocess 
+import pandas as pd
+
 Data = subprocess.check_output(['wmic', 'product', 'get', 'name']) 
 a = str(Data) 
+lista = []
 try: 
 
     for i in range(len(a)): 
         print(a.split("\\r\\r\\n")[6:][i]) 
+        lista.append(a.split("\\r\\r\\n")[6:][i])
   
 except IndexError as e: 
     print("All Done")
+    
+saida = pd.DataFrame(lista)
+
+file = 'softwares.xlsx'
+
+saida.to_excel(file)
